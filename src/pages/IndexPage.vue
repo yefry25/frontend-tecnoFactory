7dededf7ea859ef04b768e64a195b2df9b6aafb3
Jefferson Becerra <jeffersonbecerra335@gmail.com>
2023-03-09 09:07:04 -0500

    presentación

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,24 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="flex flex-center" style="height:90vh" v-bind:style="$q.screen.lt.sm ? {backgroundColor:'rgba(0, 0, 0, 0.8)'}:{width: '90vh'}">
     <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
+      fit="cover"
+      alt="Marvel"
+      src="~assets/Comic.png"
+      style="width: 100%; height: 100%"
+      v-bind:style="$q.screen.lt.sm ? {width: '100%', height: '50%'}:{width: '100%'}"
     >
   </q-page>
 </template>
 
 <script>
+import { Screen } from 'quasar'
 import { defineComponent } from 'vue'
-
+Screen.setSizes({ sm: 600, md: 800, lg: 1200, xl: 2400 });
 export default defineComponent({
   name: 'IndexPage'
 })
 </script>
+
+<style lang="scss" scoped>
+
+</style>
