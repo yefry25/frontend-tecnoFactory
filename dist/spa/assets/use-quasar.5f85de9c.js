638979a407658aed0e477e0182509105383d2456
Jefferson Becerra <jeffersonbecerra335@gmail.com>
2023-03-09 11:24:27 -0500

    cambios gráficos en la presentación

--- dist/spa/assets/use-quasar.5f85de9c.js
+++
@@ -1,1 +0,0 @@
-import{m as a,ap as r}from"./index.f9a23174.js";function u(){return a(r)}export{u};
---
+++ dist/spa/assets/use-quasar.7e24c2b9.js
@@ -0,0 +1,1 @@
+import{m as a,ap as r}from"./index.8a790509.js";function u(){return a(r)}export{u};
